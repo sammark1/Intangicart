32d72aa85762abfb72ac267f680f464f77addc46
JasonxWarren <warrenjason1@gmail.com>
2022-01-29 16:25:14 -0500

    OAuth new user

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy= require("passport-google-oauth").OAuth2Strategy;
+const User = require("../models/user");
 passport.use(
     new GoogleStrategy(
         {
@@ -8,8 +9,34 @@ passport.use(
         callbackURL: process.env.GOOGLE_CALLBACK,   
         },
         function (acessToken, refreshToken, profile, cb) {
-            console.log(profile)
+                User.findOne({ googleId: profile.id }, function (err, user) {
+                  if (err) return cb(err);
+                  if (user) {
+                    return cb(null, user);
+                  } else {
+                    // we have a new user via OAuth!
+                    const newUser = new User({ 
+                      googleId: profile.id,
+                    });
+                    newUser.save(function (err) {
+                      if (err) return cb(err);
+                      console.log(newUser.googleId)
+                      return cb(null, newUser);
+                    });
+                  }
+                });
+              }
             // a user has logged with OAuth 
-        }
+        
     )
 );
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+})
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err,) {
+        done(err, user);
+});
+});
