dea5ec2aa6956f0f3d0e2af8dc0db7c5a336cd05
Sammark1 <samaustinmark@gmail.com>
2022-01-29 12:37:46 -0500

    DB routing working

--- controllers/landingCT.js
+++ controllers/landingCT.js
@@ -2,7 +2,9 @@ const db = require("../models");
 
 const landing = (req, res) => {
     //NOTE match EJS
-    res.render("landing")
+    res.render("landing", {
+        landingTest: db.landingMD.getAll()
+    })
 }
 
 module.exports = {
