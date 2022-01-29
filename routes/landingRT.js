043629b7ac2d2536603b2fab79b24d18c7298eec
JasonxWarren <warrenjason1@gmail.com>
2022-01-29 17:56:33 -0500

    OAuth functionality

--- routes/landingRT.js
+++ routes/landingRT.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
 const ctrl = require('../controllers');
+const passport = require("passport");
 
-router.get("/", function(req, res){
-    res.redirect("/landing");
+// router.get("/", function(req, res){
+//     res.redirect("/landing");
+// });
+router.get("/", function(req,res){
+    res.render("landing", {
+        user: req.user,
+    });
 });
-router.get("/landing", ctrl.landingCT.landing)
+
+ router.get("/landing", ctrl.landingCT.landing)
+
+// Google OAuth login route
+router.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+  );
+
+router.get(
+    "/oauth2callback",
+    passport.authenticate("google", {
+      successRedirect: "/shop",
+      failureRedirect: "/",
+    })
+  );
+  router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 
 module.exports = router;
