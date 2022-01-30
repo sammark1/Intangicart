c853b937603936aeb424ef411a61f4800ff2031e
Sammark1 <samaustinmark@gmail.com>
2022-01-30 17:01:43 -0500

    changed port for process.env

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get('/', function(req, res) {
 	
 /* ====== Server bind  ====== */
 // bind the application to the port via app.listen(number, optional function to do after bind)
-app.listen(PORT, function () {
+app.listen(process.env.PORT || 4000, function () {
 	console.log(`i'm a little server live on port http://localhost:${PORT}`);
 });
