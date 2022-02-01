f871c398a7199caba6b125a07f361706512cb7f6
JasonxWarren <warrenjason1@gmail.com>
2022-01-31 19:40:42 -0500

    logout working

--- config/passport.js
+++ config/passport.js
@@ -9,15 +9,18 @@ passport.use(
         callbackURL: process.env.GOOGLE_CALLBACK,   
         },
         function (acessToken, refreshToken, profile, cb) {
+            //console.log(profile)
                 User.findOne({ googleId: profile.id }, function (err, user) {
                   if (err) return cb(err);
                   if (user) {
-                      console.log(user)
+                      //console.log(user)
                     return cb(null, user);
                   } else {
                     // we have a new user via OAuth!
                     const newUser = new User({ 
                       googleId: profile.id,
+                      //name: profile.name,
+                
                     });
                     newUser.save(function (err) {
                       if (err) return cb(err);
--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema ({
     googleId: String,
+    name: String,
 }, {
     timestamps: true,
 })
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema, "testCollection")
