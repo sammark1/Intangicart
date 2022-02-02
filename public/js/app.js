845a1919c0ebddae4a1d24f4da21989dafa89878
Sammark1 <samaustinmark@gmail.com>
2022-02-01 23:24:13 -0500

    functional popover

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,13 @@
 console.log("app.js is working");
 
+//jquery object declarations
 const $testbutton =$('#TestButton');
+const $popoverCancel =$('#popoverCancel')
 const $storecard =$('#purchaseCard');
 
 $testbutton.on("click",function(){
-    $storecard.toggle();
+    $storecard.show();
+})
+$popoverCancel.on("click",function(){
+    $storecard.hide();
 })
