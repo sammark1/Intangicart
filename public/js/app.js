d20b14dca1bd26770b5c4b999bf6d93c20dfc626
Sammark1 <samaustinmark@gmail.com>
2022-02-01 21:38:43 -0500

    card toggling is functional

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
 console.log("app.js is working");
 
 const $testbutton =$('#TestButton');
+const $storecard =$('#purchaseCard');
 
-$testbutton.on("click",function(){console.log("button clicked")})
+$testbutton.on("click",function(){
+    $storecard.toggle();
+})
