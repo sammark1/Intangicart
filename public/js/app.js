f8a364e75beb87f7b31a8a33a3d7e11e40edd929
Sammark1 <samaustinmark@gmail.com>
2022-02-07 22:34:56 -0500

    added JS control on profile editing form visibility

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ const $testbutton =$('#TestButton');
 const $popoverCancel =$('#popoverCancel')
 const $storecard =$('#purchaseCard');
 
+
 //click toggles
 $storecard.hide();
 $testbutton.on("click",function(){
@@ -13,3 +14,13 @@ $testbutton.on("click",function(){
 $popoverCancel.on("click",function(){
     $storecard.hide();
 })
+
+//Userpage
+const $editUserProfileButton =$('#profileEditButton');
+const $updateUserProfileButton =$('#profileUpdateButton');
+const $profileEdit =$('.profileUpdate');
+$profileEdit.hide();
+$editUserProfileButton.on("click",function(){
+    $profileEdit.show();
+    $editUserProfileButton.hide();
+});
